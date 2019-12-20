2b749af0eed34c209f2c9f8f6634eeba562131c7
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2019-12-20 11:21:47 -0300

    updated

--- src/database.js
+++ src/database.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
-const uristring = process.env.MONGODB_URI || 'mongodb://localhost/teslabit';
+const uristring = process.env.MONGODB_URI;
 
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/'+db, {
+console.log('Trying to connect to ' + uristring + '...');
+
 mongoose.connect(uristring, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-})
-    .then(db => console.log('Succeeded connected to: ' + uristring))
-    .catch(err => console.log('ERROR connecting to: ' + uristring + '. ' + err));
+}).then(db => console.log('Mongoose connected to: ' + uristring))
+  .catch(err => console.log('Mongoose can\'t connect with: ' + uristring + '. ' + err));
