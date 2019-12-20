322069802a20006d22277345ca7e695b71ad6ae6
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2019-12-20 10:57:26 -0300

    updated

--- src/database.js
+++ src/database.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
-const dbName = 'teslabit';
-const dbUser = 'santiagogimenez@outlook.com.ar';
-const dbPassword = 'Lilolilo10';
-const MONGODB_URI = `mongodb://${dbUser}:${dbPassword}@ds047207.mlab.com:47207/${dbName}`;
 
-const uristring =
-process.env.MONGOLAB_URI ||
-process.env.MONGOHQ_URL || MONGODB_URI;
+const uristring = 'mongodb://localhost/teslabit' || process.env.MONGODB_URI;
 
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/'+db, {
 mongoose.connect(uristring, {
