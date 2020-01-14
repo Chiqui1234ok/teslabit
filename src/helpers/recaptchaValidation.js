29b33f109a28c2ce98fc4e71e73d5c3e054c44ac
Chiqui1234 <santiagogimenez@outlook.com.ar>
2020-01-14 14:21:44 -0300

    updated

--- src/helpers/recaptchaValidation.js
+++ src/helpers/recaptchaValidation.js
@@ -1,20 +1,26 @@
 const helpers = {};
 
 helpers.recaptchaValidation = async (err, req, res, next) => {
-    let secretKey = process.env.RECAPTCHA_SECRET_KEY;
-    let token = req.body.recaptchaToken;
+    let secretKey = process.env.RECAPTCHA_SECRET_KEY | '6LfqUc4UAAAAAOCgh92z1mPmrleHIDMIluKAI-ca';
+    let { token } = req.body;
+    let result, gresponse;
     console.log(secretKey, token);
-    if(secretKey && token)
-        await fetch('`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`', {method: 'POST'})
+    if(secretKey != '' && token != '') {
+        await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`, {method: 'POST'})
         .then(result => result.json())
         .then(gresponse => res.json({ gresponse }))
         .catch(err => res.json({ err }));
-        if(err)
-            req.flash('error_msg', err);
-    else {
-        req.flash('error_msg', 'No has superado el captcha anti-robots');       
+        if(err) {
+            req.flash('error', err);
+            res.redirect('/');
+        }
+        console.log(result, gresponse);
+        next();
+    } else {
+        req.flash('error', 'No has superado el captcha anti-robots');       
+        res.redirect('/');
     }
-    next();
+    console.log('estoy fuera...', result, gresponse);
 };
 
 module.exports = helpers;
