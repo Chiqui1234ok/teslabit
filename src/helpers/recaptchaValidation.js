56d9ec1bfa5084e969af5f0910d7fc6c539b6d27
Chiqui1234 <santiagogimenez@outlook.com.ar>
2020-01-22 11:05:33 -0300

    patched

--- src/helpers/recaptchaValidation.js
+++ src/helpers/recaptchaValidation.js
@@ -2,8 +2,8 @@ const helpers = {};
 const fetch = require('node-fetch');
 
 helpers.recaptchaValidation = async (req, res, next) => {
-    let secretKey = process.env.RECAPTCHA_SECRET_KEY || '6LfqUc4UAAAAAOCgh92z1mPmrleHIDMIluKAI-ca';
-    let { token } = req.body;
+    const secretKey = process.env.RECAPTCHA_SECRET_KEY || '6LfqUc4UAAAAAOCgh92z1mPmrleHIDMIluKAI-ca';
+    const { token } = req.body;
     await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`, {method: 'GET'})
     .then(res => res.json())
     .then(( gresponse ) => {
