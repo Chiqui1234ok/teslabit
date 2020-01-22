4b7fb89f1baa239e066e8d86692dbd28d9112c37
Chiqui1234 <santiagogimenez@outlook.com.ar>
2020-01-22 13:07:55 -0300

    patched

--- src/models/transactionsBuy.js
+++ src/models/transactionsBuy.js
@@ -15,7 +15,7 @@ const TransactionsBuySch = new Schema({
     dollar: {type: Number, default: 81.9},                  // Si el usuario compró en pesos, se debe mostrar el dólar de esa fecha y usar este valor para convertir a pesos el monto final
     //date: {type: Date, default: Date.now},                  // Fecha de compra
     date: {type: String, default: '01/01/2020'},
-    finish: {type: Boolean, default: false}
+    finished: {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('transactionsBuy', TransactionsBuySch);
