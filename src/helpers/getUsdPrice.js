beecd19d4aebe301e58dbebb7ebe59662915852b
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2020-03-25 13:45:27 -0300

    updated

--- src/helpers/getUsdPrice.js
+++ src/helpers/getUsdPrice.js
@@ -2,15 +2,15 @@ const fetch = require('node-fetch');
 const helpers = {};
 
 helpers.getUsdPrice = async function() { // no-optimized
-    let value;
-    await fetch('https://argentina-hoy.herokuapp.com/devs/dolar-hoy', {method: 'GET'})
-    .then(res => res.json())
-    .then((data) => {
-        value = data.sell.official*1.3; // impuesto de 30%
-    });
-    // .catch(console.warn('No se pudo obtener el dólar hoy.'));
-    //return value;
-    return 81.9;
+    // let value;
+    // await fetch('https://argentina-hoy.herokuapp.com/devs/dolar-hoy', {method: 'GET'})
+    // .then(res => res.json())
+    // .then((data) => {
+    //     value = data.sell.official*1.3; // impuesto de 30%
+    // });
+    // // .catch(console.warn('No se pudo obtener el dólar hoy.'));
+    // //return value;
+    return 86;
 }
 
 module.exports = helpers;
